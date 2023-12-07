f765a9ac6d979dc946d9b9792f7420a8e9399a65
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-12-07 13:43:52 +0000

    fetched API data and displayed in UI

--- src/app/pages/media.tsx
+++ src/app/pages/media.tsx
@@ -4,18 +4,23 @@ const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
 // Define a TypeScript interface for the movie object
 interface Movie {
-  type: string;
-  name: string;
-  year: string;
-  runtime: number;
-  imageUrl: string;
+  original_title: string;
+  release_date: string;
+  vote_average: number;
+  poster_path: string;
 }
 
 const MoviePage = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
   const getMovie = () => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
       .then((res) => res.json())
-      .then((json) => console.log(json));
+      .then((data) => {
+        if (data && data.results) {
+          setMovies(data.results);
+        }
+      });
   };
 
   useEffect(() => {
@@ -24,7 +29,17 @@ const MoviePage = () => {
 
   return (
     <div>
-      <h1>Check the console for movie data</h1>
+      {movies.map((movie, i) => (
+        <div key={i}>
+          <h2>{movie.original_title}</h2>
+          <p>Release Date: {movie.release_date}</p>
+          <p>Rating: {movie.vote_average}</p>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.original_title}
+          />
+        </div>
+      ))}
     </div>
   );
 };
