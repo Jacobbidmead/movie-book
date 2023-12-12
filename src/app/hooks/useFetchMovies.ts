2c12c3799a874481ffacecd8eb33c0ac9b987fce
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-12-12 13:12:15 +0000

    refactor: moved fetch logic into custom hook and imported to media view

--- src/app/hooks/useFetchMovies.ts
+++ src/app/hooks/useFetchMovies.ts
@@ -1,3 +1,43 @@
 "use client";
 
 import { useState, useCallback } from "react";
+
+const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+
+export const useFetchMovies = () => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMovies = useCallback((query = "") => {
+    setIsLoading(true);
+    setError(null);
+
+    const url = query
+      ? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+          query
+        )}`
+      : `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch movies");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data && data.results) {
+          setMovies(data.results);
+        }
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  return { movies, isLoading, error, fetchMovies };
+};
--- src/app/pages/media.tsx
+++ src/app/pages/media.tsx
@@ -2,54 +2,13 @@
 
 import React, { useEffect, useState, useCallback } from "react";
 import Search from "../components/search";
-
-const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
-
-interface Movie {
-  original_title: string;
-  release_date: string;
-  vote_average: number;
-  poster_path: string;
-}
+import { useFetchMovies } from "../hooks/useFetchMovies";
 
 const MoviePage = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  const fetchMovies = (query: string = "") => {
-    setIsLoading(true);
-    setError(null);
-
-    const url = query
-      ? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
-          query
-        )}`
-      : `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
-
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Failed to fetch movies");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        if (data && data.results) {
-          setMovies(data.results);
-        }
-      })
-      .catch((error) => {
-        setError(error.message);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
+  const { movies, isLoading, error, fetchMovies } = useFetchMovies();
   return (
     <div>
       <Search onSearch={fetchMovies} />
-
       {movies.map((movie, i) => (
         <div key={i}>
           <h2>{movie.original_title}</h2>
