1cb79dc53406b4f5fe872a6d7a88e82f400947d2
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-12-12 12:57:12 +0000

    refactor: created search component to refactor code structure
    refactoring

---
+++ src/app/components/search.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+interface SearchProps {
+  onSearch: (query: string) => void;
+}
+
+const Search: React.FC<SearchProps> = () => {};
+
+export default Search;
