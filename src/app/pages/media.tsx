d1db1a4b1b60d8f41b2569e8934b5295bfe655d5
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-12-15 12:52:51 +0000

    feat: added feature that will allow users to saved movies

--- src/app/pages/media.tsx
+++ src/app/pages/media.tsx
@@ -1,11 +1,39 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Search from "../components/search";
 import { useFetchMovies } from "../hooks/useFetchMovies";
 
+interface savedMovies {
+  saveMovie: string;
+}
+
+interface Movie {
+  original_title: string;
+  release_date: string;
+  vote_average: string;
+  id: string;
+  poster_path: string;
+}
+
 const MoviePage: React.FC = () => {
+  const [savedMovie, setSavedMovie] = useState<Movie[]>([]);
+
+  const addMovie = (movieToSave: Movie) => {
+    setSavedMovie((prevSavedMovies) => {
+      // Check if the movie already exists in the saved list
+      const isMovieSaved = prevSavedMovies.some(
+        (movie) => movie.id === movieToSave.id
+      );
+      if (!isMovieSaved) {
+        return [...prevSavedMovies, movieToSave]; // Add new movie
+      }
+      return prevSavedMovies; // Return existing list if movie is already saved
+    });
+  };
+
   const { movies, isLoading, error, fetchMovies } = useFetchMovies();
+
   return (
     <div>
       <Search onSearch={fetchMovies} />
@@ -18,8 +46,11 @@ const MoviePage: React.FC = () => {
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={movie.original_title}
           />
+          <button onClick={() => addMovie(movie)}>Save</button>
         </div>
       ))}
+
+      <div></div>
     </div>
   );
 };
