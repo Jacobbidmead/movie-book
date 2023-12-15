f469d443e9095785271664cd2fbde97cae242bd2
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-12-15 13:14:32 +0000

    refactor: moved saved movies into own component and passed function as props

--- src/app/pages/media.tsx
+++ src/app/pages/media.tsx
@@ -3,13 +3,15 @@
 import React, { useState } from "react";
 import Search from "../components/search";
 import { useFetchMovies } from "../hooks/useFetchMovies";
+import UserMedia from "../components/userMedia";
 import { Movie } from "../types/interfaces";
 
 const MoviePage: React.FC = () => {
-  const [savedMovie, setSavedMovie] = useState<Movie[]>([]);
+  const { movies, isLoading, error, fetchMovies } = useFetchMovies();
+  const [savedMovies, setSavedMovies] = useState<Movie[]>([]);
 
   const addMovie = (movieToSave: Movie) => {
-    setSavedMovie((prevSavedMovies) => {
+    setSavedMovies((prevSavedMovies) => {
       // Check if the movie already exists in the saved list
       const isMovieSaved = prevSavedMovies.some(
         (movie) => movie.id === movieToSave.id
@@ -20,8 +22,6 @@ const MoviePage: React.FC = () => {
       return prevSavedMovies; // Return existing list if movie is already saved
     });
   };
-
-  const { movies, isLoading, error, fetchMovies } = useFetchMovies();
 
   return (
     <div>
@@ -38,19 +38,7 @@ const MoviePage: React.FC = () => {
           <button onClick={() => addMovie(movie)}>Save</button>
         </div>
       ))}
-
-      {savedMovie.map((savedMovie) => (
-        <div key={savedMovie.id}>
-          {" "}
-          <h2>{savedMovie.original_title}</h2>
-          <p>Release Date: {savedMovie.release_date}</p>
-          <p>Rating: {savedMovie.vote_average}</p>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${savedMovie.poster_path}`}
-            alt={savedMovie.original_title}
-          />
-        </div>
-      ))}
+      <UserMedia savedMovies={savedMovies} />
     </div>
   );
 };
