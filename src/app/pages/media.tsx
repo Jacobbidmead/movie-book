a7c74d46aefc570378e7190759d8a4a3add258e6
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-12-15 13:00:36 +0000

    feat: displayed saved movies

--- src/app/pages/media.tsx
+++ src/app/pages/media.tsx
@@ -39,7 +39,18 @@ const MoviePage: React.FC = () => {
         </div>
       ))}
 
-      <div></div>
+      {savedMovie.map((savedMovie) => (
+        <div key={savedMovie.id}>
+          {" "}
+          <h2>{savedMovie.original_title}</h2>
+          <p>Release Date: {savedMovie.release_date}</p>
+          <p>Rating: {savedMovie.vote_average}</p>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${savedMovie.poster_path}`}
+            alt={savedMovie.original_title}
+          />
+        </div>
+      ))}
     </div>
   );
 };
