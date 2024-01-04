662ad9e5df6117db8900f897a651f1d8819dd02a
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-01-04 22:43:27 +0000

    refactor: updated db connect code
    moved global.d.ts into types folder & adjusted tsconfig to include it globally

--- src/app/lib/dbConnect.tsx
+++ src/app/lib/dbConnect.tsx
@@ -6,29 +6,25 @@ if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
 }
 
+const mongooseCache: MongooseCache = { conn: null, promise: null };
+
 async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
+  if (mongooseCache.conn) {
+    return mongooseCache.conn;
   }
 
-  if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+  if (!mongooseCache.promise) {
+    mongooseCache.promise = mongoose.connect(MONGODB_URI!).then((mongoose) => {
       return mongoose;
     });
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
+  mongooseCache.conn = await mongooseCache.promise;
+  return mongooseCache.conn;
 }
 
 export default dbConnect;
